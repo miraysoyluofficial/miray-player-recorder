d15face871e9ccc090fec7ad66f9c3826f110d88
miraysoyluofficial <miraysoyluofficial@gmail.com>
2026-06-06 18:07:20 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "miray-player-recorder-v5";
+const CACHE_NAME = "miray-player-recorder-v6";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -34,6 +34,19 @@ self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
   const requestUrl = new URL(event.request.url);
   if (requestUrl.origin !== self.location.origin) return;
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put("./index.html", copy));
+          return response;
+        })
+        .catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
 
   event.respondWith(
     caches.match(event.request).then((cached) => {
